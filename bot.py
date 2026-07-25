e19efdee4859fc9b0f4b378d4825a2e86b14f015
rahulkumar950769-byte <rahulkumar950769@gmail.com>
2026-07-25 05:34:06 +0530

    updata bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import os
+import google.generativeai as genai
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+
+genai.configure(api_key=GEMINI_API_KEY)
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Heena Bot Online hai 😊 Kya help chahiye?")
+
+async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_msg = update.message.text
+    response = model.generate_content(user_msg)
+    await update.message.reply_text(response.text)
+
+print("Bot started...")
+app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
+app.run_polling()
