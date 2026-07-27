c19a45ee9163f04a5e3ae76c911430ce2b4d6181
rahulkumar950769-byte <rahulkumar950769@gmail.com>
2026-07-28 02:44:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,12 @@
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-import google.generativeai as genai
+from google import genai
 
-# Token yaha dalna hai ya Render ke Environment me
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-# Gemini setup
-genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Namaste! Main Heenabot hu 🤖\nKuch bhi pucho, main jawab dunga.")
@@ -17,7 +14,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_text = update.message.text
     try:
-        response = model.generate_content(user_text)
+        response = client.models.generate_content(model="gemini-2.0-flash", contents=user_text)
         await update.message.reply_text(response.text)
     except Exception as e:
         await update.message.reply_text(f"Error: {e}")
